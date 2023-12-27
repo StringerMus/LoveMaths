7cf3dba1a1f4037102227cc17660a002b2048713
StringerMus <>
2023-12-27 22:04:25 +0000

    add event listener instruction in script.js and add commands for runGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,31 @@
+//wait for the DOM to finish loading before running the game
+//Get the button elements and event listeners to them
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementByTagName("button");
+
+    for (let button of buttons){
+        button.addEventListener("click",function() {
+            if (this.getAttribute("data-type")==="submit"){
+                alert("You clicked Submit!");
+            } else {
+                let gameType= this.getAttribute("data-type");
+                alert(`You clicked ${gametype}`);
+            }
+        })
+    }
+})
+
+/**
+* The main game "loop", called when the script is first loaded
+*and after the user's answer has been process
+*/
 function runGame(){
 
+    //Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random()*25) + 1;
+    let num2 = Math.floor(Math.random()*25) + 1;
 }
-
 
 function checkAnswer(){
     
